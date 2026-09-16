5df8c40bab2f810f1fc1ba6ffa27905451b887ac
Carl H <128645022+cccarllll@users.noreply.github.com>
2026-09-16 11:07:39 +0200

    temp
    temp

--- main_branch.py
+++ main_branch.py
@@ -2,9 +2,12 @@ import csv
 import random
 
 
-temperature_celsius = round(random.uniform(-10, 50), 1)
+temperatures_celsius = [
+    round(random.uniform(-10, 50), 1)
+    for _ in range(10)
+]
 
-with open("temperature.csv", "w", newline="", encoding="utf-8") as csv_file:
+with open("data.csv", "w", newline="", encoding="utf-8") as csv_file:
     writer = csv.writer(csv_file)
     writer.writerow(["temperature_celsius"])
-    writer.writerow([temperature_celsius])
+    writer.writerows([[temperature] for temperature in temperatures_celsius])
